935e47228ba2ae247520146b83e7ad1ce6887dc8
Miramac <miramac@gmx.net>
2014-12-11 15:52:47 +0100

    add batch input

--- test/sandbox.js
+++ test/sandbox.js
@@ -8,15 +8,30 @@ writer = new Writer({
 	'db': dbPath
 });
 
-writer.write([ { ID: 1, name: 'UnitA', result: 100 }, { ID: 2, name: 'UnitB', result: 100} ], [ 'ID', 'name' ]);
+writer.write([ { ID: 1, name: 'UnitA', result: 1000 }, { ID: 1, name: 'UnitB', result: 100}, { ID: 2, name: 'UnitA', result: 100} ], [ 'ID', 'name' ], TestData);
 
-// var entries = []
+//TestData()
+function TestData() {
+    db = level(dbPath);
+    db.get('1\x00UnitA', function (err, value) {
+        console.log('1~UnitA:', value)
+    });
+    
+    var allData = [];
+    db.createReadStream({
+        start : '1'        
+      , end   : '1\x00UnitA\xff' // stop at the last key with the prefix
+    })
+    .on('data', function (data) {
+      allData.push(data.value);
+    })
+    .on('close', function () {
+      console.log(allData);
+    }) 
+    
+    
+}
 
-// db = level(dbPath);
- // db.get('ID', function (err, value) {
-        // console.log('dprk:', value)
-        // db.close()
-      // })
 // db.createReadStream({ start: 'UnitA\x00', end: 'UnitA\x00\xff' })
   // .on('data', function (entry) { entries.push(entry) })
   // .on('close', function () { console.log(entries) })
