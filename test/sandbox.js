a9f88b5513fa7f2b437d3163e2399cb6cdb34f20
Miramac <miramac@gmx.net>
2015-02-19 21:26:25 +0100

    update to datakey

--- test/sandbox.js
+++ test/sandbox.js
@@ -5,21 +5,21 @@ var path = require('path')
 , dbPath = path.join(__dirname, 'db/test')
 ;
 
-level.destroy(dbPath);
+//level.destroy(dbPath);
 
 writer = new Writer({
 	'db': dbPath
 });
 
-writer.write([ { ID: 1, name: 'UnitA', result: 1000 }, { ID: 1, name: 'UnitB', result: 100}, { ID: 2, name: 'UnitA', result: 100} ], [ 'ID', 'name' ], TestData);
+ writer.write([ { ID: 1, name: 'UnitA', result: 600 }, { ID: 1, name: 'UnitB', result: 400}, { ID: 2, name: 'UnitA', result: 500}, { ID: 3, name: 'UnitA', result: 200} ], [ 'ID', 'name' ], TestData);
 
 //TestData()
 function TestData() {
     db = level(dbPath);
     var allData = [];
     db.createReadStream({
-        start : '1'        
-      , end   : '1\x00Unit\xff' // stop at the last key with the prefix
+        start : '\x00'        
+      , end   : '\xff'
     })
     .on('data', function (data) {
       allData.push(data.value);
@@ -29,9 +29,3 @@ function TestData() {
       console.log(allData);
     }) 
 }
-
-
-// db.createReadStream({ start: 'UnitA\x00', end: 'UnitA\x00\xff' })
-  // .on('data', function (entry) { entries.push(entry) })
-  // .on('close', function () { console.log(entries) })
-
